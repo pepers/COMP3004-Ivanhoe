1820f5f307b469171b77c00636dd9e10fe94ac35
Matthew Pepers <matt.pepers@gmail.com>
2016-02-02 17:38:36 -0500

    updated gitignore for Eclipse projects, skeleton classes for Client and ClientTest
    set up directory structure also

---
+++ src/test/resources/ConfigTest.java
@@ -0,0 +1,14 @@
+package test.resources;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ConfigTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
