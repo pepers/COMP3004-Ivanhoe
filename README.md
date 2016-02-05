28d73c75bbaf49995cc00247f4317896df97c547
Matthew Pepers <matt.pepers@gmail.com>
2016-02-05 12:33:32 -0500

    fixed log4j jar file, basic trace works

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 group project
 
 ---
-### Installation:
+### Installation / Setup:
 - have the project open in Eclipse
 - navigate to /src/test/resources/
-- right-click on the following files, go to 'Build Path' -> 'Add to Build Path'
+- right-click on the following files, go to 'Build Path' -> 'Add to Build Path':
   - hamcrest-core-1.3.jar
   - junit-4.12.jar
-  - log4j-api-2.5.jar
+  - log4j-1.2.17.jar
--- src/main/resources/Trace.java
+++ src/main/resources/Trace.java
@@ -3,8 +3,19 @@
  */
 package main.resources;
 
-import org.apache.logging.log4j.*;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+import java.io.*;
+import java.sql.SQLException;
 
 public class Trace {
-
+	/* Get actual class name to be printed on */
+	final static Logger log = Logger.getLogger(Trace.class.getName());
+	   
+	public static void main(String[] args)throws IOException,SQLException{
+		PropertyConfigurator.configure("logs/log4j.properties");
+		log.debug("Hello this is a debug message");
+		log.info("Hello this is an info message");
+	}
 }
