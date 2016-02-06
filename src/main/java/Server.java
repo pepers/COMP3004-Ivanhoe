c516a05954454d9ef6d759911687ae002578fb9c
khalilva <khalil.va@gmail.com>
2016-02-06 14:37:16 -0500

    Added shutdown logic.

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -2,6 +2,7 @@ package main.java;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.Set;
 
 public class Server {
 	
@@ -17,6 +18,14 @@ public class Server {
 			System.out.println("Binding to port " + port + ", please wait  ...");
 			serverSocket = new ServerSocket(port);
 			serverSocket.setReuseAddress(true);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void shutdown(){
+		try {
+			serverSocket.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
