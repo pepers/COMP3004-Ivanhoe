1b25a1d7f1d6f5de36dc3f550a8a9116f0b9d478
khalilva <khalil.va@gmail.com>
2016-02-06 14:25:51 -0500

    Added Server test file

--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -1,7 +1,6 @@
 package test.java;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class ClientTest {
---
+++ src/test/java/ServerTest.java
@@ -0,0 +1,12 @@
+package test.java;
+
+import org.junit.Test;
+
+public class ServerTest {
+	
+	
+	@Test
+	public void TestInitialize(){
+		Server s = new Server();
+	}
+}
