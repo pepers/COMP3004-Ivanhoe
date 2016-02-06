747a7ffb696f17afef8194348fe290337b203112
khalilva <khalil.va@gmail.com>
2016-02-06 14:29:41 -0500

    Logic rewrite for server testing

--- src/test/java/ServerTest.java
+++ src/test/java/ServerTest.java
@@ -1,12 +1,22 @@
 package test.java;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ServerTest {
 	
+	Server s = new Server();
 	
-	@Test
-	public void TestInitialize(){
-		Server s = new Server();
+	@Before
+	public void before(){
+		s.startup();
+	}
+	
+	
+	
+	@After
+	public void after(){
+		s.shutdown();
 	}
 }
