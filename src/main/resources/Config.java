78872260f05d1986ef60a349317665ad4b987b16
khalilva <khalil.va@gmail.com>
2016-02-06 14:35:33 -0500

    Added a default port

--- src/main/resources/Config.java
+++ src/main/resources/Config.java
@@ -1,5 +1,5 @@
 package main.resources;
 
 public class Config {
-
+	public static int DEFAULT_PORT = 5050;
 }
--- src/test/java/ServerTest.java
+++ src/test/java/ServerTest.java
@@ -4,9 +4,12 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import main.java.Server;
+import main.resources.Config;
+
 public class ServerTest {
 	
-	Server s = new Server();
+	Server s = new Server(Config.DEFAULT_PORT);
 	
 	@Before
 	public void before(){
