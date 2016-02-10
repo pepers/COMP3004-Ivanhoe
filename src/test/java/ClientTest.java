6d91a00642c1c1f2f2146974e912f09e7bad8cc9
Matthew Pepers <matt.pepers@gmail.com>
2016-02-10 11:18:14 -0500

    Client connection test

--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -1,17 +1,27 @@
 package test.java;
 
-import static org.junit.Assert.*;
-import org.junit.Test;
+import org.junit.*;
 
 import main.java.Client;
 import main.resources.Config;
 
 public class ClientTest {
 
+	Client c = null;
+	
+	@Before
+	public void setUp() {
+		c = new Client();
+	}
+	
+	@After
+	public void tearDown () {
+		c = null;
+	}
+
 	@Test
 	public void connect() {
-		Client c = new Client();
-		c.connect("localhost", Config.DEFAULT_PORT);
+		c.connect(Config.DEFAULT_HOST, Config.DEFAULT_PORT);
 	}
 
 }
