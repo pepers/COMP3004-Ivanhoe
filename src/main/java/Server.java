4c399a61803c3bde20045172c09ee51734dc2146
khalilva <khalil.va@gmail.com>
2016-02-12 15:14:05 -0500

    Rewrote logging for serverclass

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -6,6 +6,7 @@ import java.net.Socket;
 import java.util.HashMap;
 
 import main.resources.Config;
+import main.resources.Trace;
 
 public class Server implements Runnable{
 	
@@ -28,7 +29,7 @@ public class Server implements Runnable{
 	
 	public void startup(){
 		try {
-			System.out.println("Binding to port " + port + ", please wait  ...");
+			Trace.getInstance().write(this, "Binding to port " + port + ", please wait  ...");
 			serverSocket = new ServerSocket(port);
 			serverSocket.setReuseAddress(true);
 			
@@ -41,22 +42,22 @@ public class Server implements Runnable{
 	}
 	
 	private void addThread(Socket socket) {
-		System.out.println("Client Requesting connection: " + socket.getPort());
+		Trace.getInstance().write(this, "Client Requesting connection: " + socket.getPort());
 		if (numClients < Config.MAX_PLAYERS) {
 			ServerThread serverThread = new ServerThread(this, socket);
 			serverThread.start();
 			clients.put(serverThread.getID(), serverThread);
 			this.numClients++;
-			System.out.println("Client Accepted: " + socket.getPort());
+			Trace.getInstance().write(this, "Client Accepted: " + socket.getPort());
 		} else {
-			System.out.println("Client Tried to connect:" + socket.getLocalSocketAddress());
-			System.out.println("Client refused: maximum number of clients reached ("+ numClients +")");
+			Trace.getInstance().write(this, "Client Tried to connect:" + socket.getLocalSocketAddress());
+			Trace.getInstance().write(this, "Client refused: maximum number of clients reached ("+ numClients +")");
 		}
 	}
 	
 	public void shutdown(){
 		try {
-			System.out.println("Shutting down server @ " + port + ", please wait  ...");
+			Trace.getInstance().write(this, "Shutting down server @ " + port + ", please wait  ...");
 			search = false;
 			searchThread = null;
 			
