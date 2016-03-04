857c25f6d8df349889fa575623395b33fb67e307
Matthew Pepers <matt.pepers@gmail.com>
2016-03-04 17:02:24 -0500

    DisplayCard toString() now returns squire or maiden for none cards

--- src/main/java/DisplayCard.java
+++ src/main/java/DisplayCard.java
@@ -7,7 +7,7 @@ public class DisplayCard extends Card{
 	private int value;      // point value of the card
 	private Colour colour;  // colour of the card
 	
-	// possible card colours (none is for squires and maidens)
+	// possible card colours (none is for squires(2,3) and maidens(6))
 	public enum Colour {none, purple, red, blue, yellow, green}
 	
 	public DisplayCard (int value, DisplayCard.Colour colour) {
@@ -16,7 +16,18 @@ public class DisplayCard extends Card{
 	}
 	
 	public String toString(){
-		return (colour.name() + ":" + value);
+		// SQUIRE
+		if ((this.colour.name().equals("none")) && ((this.value == 2) || (this.value == 3))) {
+			return "squire:" + this.value;
+			
+		// MAIDEN
+		} else if ((this.colour.name().equals("none")) && (this.value == 6)) {
+			return "maiden:" + this.value;
+			
+		// COLOUR CARDS
+		} else {
+			return (this.colour.name() + ":" + this.value);
+		}
 	}
 
 	public String getColour() {
