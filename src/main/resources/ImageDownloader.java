459220c34d230f82da3fc9bd4d5cb27c481ab559
khalilva <khalil.va@gmail.com>
2016-03-08 23:23:31 -0500

    Changed some logic, added action card downloader

--- src/main/resources/ImageDownloader.java
+++ src/main/resources/ImageDownloader.java
@@ -1,10 +1,8 @@
 package main.resources;
 
 import java.awt.Image;
-import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.awt.image.RenderedImage;
-import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -12,56 +10,90 @@ import javax.imageio.ImageIO;
 
 public class ImageDownloader {
 	
-	static String home = "http://people.scs.carleton.ca/~jeanpier//304W16/project%20material/";
-	
 	public static void main(String[] args){
 		ImageDownloader d = new ImageDownloader();
-		d.fetchDisplayCards();
+		d.fetchActionCards();
+	}
+	
+	public void fetchCards(String[] names, String path, String folder){
+		URL url;
+		Image image = null;
+		System.out.println("Downloading to ("+ folder +")...");
+		int count = 0;
+		for (int i = 0; i < names.length; i++){
+			image = null;
+			try {
+				url = new URL(path + ((i==0) ? "":i) + ".jpeg");
+			    image = ImageIO.read(url.openStream());
+			    BufferedImage imageCropped = rotate90(((BufferedImage) image).getSubimage(9, 20, 985, 692));
+			    File f = new File("./res/"+folder+"/" + names[i] + ".png");
+			    f.mkdirs();
+			    ImageIO.write((RenderedImage) imageCropped, "png", f);
+			    System.out.println("  " + names[i] + " - s");
+			    count++;
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.out.println("  " + names[i] + " - f");
+			}
+		}
+		System.out.println("Done! Fetched (" + count + "/"+names.length+")");
 	}
 	
 	public void fetchDisplayCards(){
-		URL url;
-		Image image = null;
-		
-		//get supporters
-		for (int i = 0; i < 18; i++){
-			image = null;
-			try {
-				url = new URL(home + "simpleCards/simpleCards" + ((i==0) ? "":i) + ".jpeg");
-			    image = ImageIO.read(url.openStream());
-			    BufferedImage imageCropped = ((BufferedImage) image).getSubimage(9, 20, 985, 692);
-
-			    imageCropped = rotate90(imageCropped);
-			    
-			    File f = new File("./res/displaycards/" + displayNames[i] + ".png");
-			    System.out.println(f.getAbsolutePath());
-			    ImageIO.write((RenderedImage) imageCropped, "png", f);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			System.out.println(image);
-		}
+		String[] displayNames = {
+				"squire2",
+				"squire3",
+				"maiden6",
+				"green1",
+				"yellow2",
+				"yellow3",
+				"yellow4",
+				"red3",
+				"red4",
+				"red5",
+				"blue2",
+				"blue3",
+				"blue4",
+				"blue5",
+				"purple3",
+				"purple4",
+				"purple5",
+				"purple7"
+		};
+		fetchCards(
+				displayNames,
+				"http://people.scs.carleton.ca/~jeanpier//304W16/project%20material/simpleCards/simpleCards",
+				"displaycards");
 	}
-	String[] displayNames = {
-			"squire2",
-			"squire3",
-			"maiden6",
-			"green1",
-			"yellow2",
-			"yellow3",
-			"yellow4",
-			"red3",
-			"red4",
-			"red5",
-			"blue2",
-			"blue3",
-			"blue4",
-			"blue5",
-			"purple3",
-			"purple4",
-			"purple5",
-			"purple7"
-	};
+	
+	public void fetchActionCards(){
+		String[] actionNames = {
+				"dodge",
+				"disgrace",
+				"retreat",
+				"riposte",
+				"outmaneuver",
+				"countercharge",
+				"charge",
+				"breaklance",
+				"adapt",
+				"dropweapon",
+				"changeweapon",
+				"unhorse",
+				"knockdown",
+				"outwit",
+				"shield",
+				"stunned",
+				"ivanhoe"
+		};
+		fetchCards(
+				actionNames,
+				"http://people.scs.carleton.ca/~jeanpier//304W16/project%20material/actionCards/actionCards",
+				"actioncards");
+	}
+	
+	
+	
 	public BufferedImage rotate90(BufferedImage bi) {
 	    int width = bi.getWidth();
 	    int height = bi.getHeight();
